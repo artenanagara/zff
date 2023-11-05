b33981cc993cf91258cca17361de42782b14ef34
Artena <artenadhytyan22@gmail.com>
2023-11-06 01:35:53 +0700

    change font weight

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
                 <div className="flex flex-col items-center gap-8">
                     <p className="text-lg">Welcome to team</p>
                     <div className="flex flex-col gap-5 text-center">
-                        <h1 className="text-8xl"><span className="text-primary">Always</span> The Real Thing,<br />Always <span className="text-primary">Digital.</span></h1>
+                        <h1 className="text-8xl font-bold"><span className="text-primary">Always</span> The Real Thing,<br />Always <span className="text-primary">Digital.</span></h1>
                         <p>We create products & innovations that brings positive shift to the world</p>
                     </div>
                     <a href="#" className="flex justify-between items-center font-semibold text-base gap-4 py-2 px-8 rounded-full bg-primary">Hire Us <LiaLongArrowAltRightSolid className='text-4xl' /></a>
